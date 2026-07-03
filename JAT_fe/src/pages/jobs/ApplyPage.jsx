e0c2e0c39f667325237c80f3f9a78149d9619541
gedeno <gedenogeremew@gmail.com>
2026-07-03 22:29:46 +0300

    applay page are created and with some editons

--- JAT_fe/src/pages/jobs/ApplyPage.jsx
+++ JAT_fe/src/pages/jobs/ApplyPage.jsx
@@ -1,1 +1,45 @@
 import api from "../auth/api";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const Cv_apply = () => {
+    const [formdata , setformdata] = useState({})
+    const inputhandler = (e) =>{
+        setformdata({
+            ...formdata,
+            [e.target.name]:e.target.value
+        })
+    }
+    const filehander = (e) => {
+        const file = e.target.files?.[0] ??null
+        setformdata({
+            ...formdata,
+            [e.target.name]:file
+        })
+    }
+    const naviget = useNavigate()
+    const submithandler = (e) => {
+        api.post('/applis/apply/',formdata)
+        .then((res) => {
+            console.log(res.data)
+            naviget('/home')
+        })
+    }
+    return(
+        <>
+        <div>
+            <form action="">
+                <label htmlFor="">cover leter:</label>
+                <input type="text" name="cover_letter" id="" /><br />
+                <label htmlFor="">file</label>
+                <img src="file" alt="" name = "cv_resume" /> <br />
+                <label htmlFor="">portfolio link : </label>
+                <input type="text" name="portfolio_link" /> <br />
+                <label htmlFor="">github link :</label>
+                <input type="text" name="github_link" />
+                <button>apply</button>
+            </form>
+        </div>
+        </>
+    )
+
+}
