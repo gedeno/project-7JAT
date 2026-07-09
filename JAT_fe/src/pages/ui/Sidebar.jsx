3acb5dc6b93debd4b1242fcd5f04eb5481b3f6d8
gedeno <gedenogeremew@gmail.com>
2026-07-09 23:20:23 +0300

    small edtions and some pages are adding

---
+++ JAT_fe/src/pages/ui/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+import {HiOutlineViewGrid , 
+    HiOutlineBriefcase , 
+    HiOutlineChatAlt2 ,
+    HiOutlineUser ,
+    HiOutlineCog ,
+    HiOutlineShieldCheck , 
+    HiOutlineDocument } from 'react-icons/hi'
+export const Sidebar = () =>{
+    const {}
+}
