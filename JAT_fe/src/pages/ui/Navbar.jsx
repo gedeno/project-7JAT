4a7457673570bceecbc1fb5796305d387e7c58f4
gedeno <gedenogeremew@gmail.com>
2026-07-09 23:14:39 +0300

    small edtions and some pages are adding

---
+++ JAT_fe/src/pages/ui/Navbar.jsx
@@ -0,0 +1,8 @@
+import {NavLink} from 'react-router-dom'
+import {HiOutlineViewGrid , 
+    HiOutlineBriefcase , 
+    HiOutlineChatAlt2 ,
+    HiOutlineUser ,
+    HiOutlineCog ,
+    HiOutlineShieldCheck , 
+    HiOutlineDocument } from 'react-icons/hi'
