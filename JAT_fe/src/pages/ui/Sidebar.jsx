0995462f00056479a60c4f4eda47a574f3229920
gedeno <gedenogeremew@gmail.com>
2026-07-10 22:15:29 +0300

    some pages are created

--- JAT_fe/src/pages/ui/Sidebar.jsx
+++ JAT_fe/src/pages/ui/Sidebar.jsx
@@ -20,32 +20,28 @@ const NavItems =  [
 export const Sidebar = () =>{
     const [isopen , setIsopen] = useState(true)
     return (
-        <>
-        {isopen&&
-        <div>
-
-            </div>}
+      <>
+        {isopen && <div></div>}
         <aside
-        className= {`fixed lg:static inset-y-0 left-0 z-40 w-64 bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-800 transform transtion-transform duration-300 lg:translate-x-0 ${isopen ? 'translate-x-0' : '-translate-x-full'}`}>
-            <div className="flex flex-col h-full pt-4 lg:pt-6">
-                <nav className="flex-1 px-3 space-y-1 ">
-                    {
-                        NavItems.map(({label , icon : Icon}) => (
-                            <NavLink className= 'flex items-center gap-3 px-3 py-2.5 rounded-lg text-sm font-medium transition-colors taxt-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-800 '>
-                                <Icon size={20}/>
-                                {label}
-                            </NavLink>
-
-                        ))
-                    }
-                </nav>
-                <div className="p-4 border-t border-gray-200 dark:border-gray-800" >
-                    <NavLink className = "flex items-center gap-3 justify-center w-full px-4 py-2.5 rounded-lg bg-primary-600 hover:bg-primary-700 text-white text-sm font-medium transition-colors "
-                    > <HiOutlineDocument size={20}/> post a job </NavLink>
-                </div>
+          className={`fixed lg:static inset-y-0 left-0 z-40 w-64 bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-800 transform transtion-transform duration-300 lg:translate-x-0 ${isopen ? "translate-x-0" : "-translate-x-full"}`}
+        >
+          <div className="flex flex-col h-full pt-4 lg:pt-6">
+            <nav className="flex-1 px-3 space-y-1 ">
+              {NavItems.map(({ label, icon: Icon }) => (
+                <NavLink className="flex items-center gap-3 px-3 py-2.5 rounded-lg text-sm font-medium transition-colors taxt-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-800 ">
+                  <Icon size={20} />
+                  {label}
+                </NavLink>
+              ))}
+            </nav>
+            <div className="p-4 border-t border-gray-200 dark:border-gray-800">
+              <NavLink className="flex items-center gap-3 justify-center w-full px-4 py-2.5 rounded-lg bg-primary-600 hover:bg-primary-700 text-white text-sm font-medium transition-colors ">
+                {" "}
+                <HiOutlineDocument size={20} /> post a job{" "}
+              </NavLink>
             </div>
+          </div>
         </aside>
-
-        </>
-    )
+      </>
+    );
 }
